dbbff30e6ec1ed02566d862fc5ded104e9272234
Felix Locquet <felix.locquet@outlook.fr>
2026-04-05 23:34:23 -0200

    Exercice avec prompt et tableau de mots à écrire

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Hello, World! This is a simple JavaScript file.");
+const listeMots = ["Cachalot", "Pétunia", "Serviette"]
+
+let score = 0
+
+let motUtilisateur = prompt("Entrez le premier mot du tableau :")
+
+if (motUtilisateur === listeMots[0]) {
+    score++
+    alert("Bravo ! Vous avez trouvé le mot " + listeMots[0] + " ! Votre score est de " + score)
+} else {
+    alert("Dommage, ce n'est pas le bon mot. Le mot était " + listeMots[0] + ". Votre score est de " + score)
+}
+
+motUtilisateur = prompt("Entrez le deuxième mot du tableau :")
+
+if (motUtilisateur === listeMots[1]) {
+    score++
+    alert("Bravo ! Vous avez trouvé le mot " + listeMots[1] + " ! Votre score est de " + score)
+} else {
+    alert("Dommage, ce n'est pas le bon mot. Le mot était " + listeMots[1] + ". Votre score est de " + score)
+}
+
+motUtilisateur = prompt("Entrez le troisième mot du tableau :")
+
+if (motUtilisateur === listeMots[2]) {
+    score++   
+    alert("Bravo ! Vous avez trouvé le mot " + listeMots[2] + " ! Votre score est de " + score)
+} else {
+    alert("Dommage, ce n'est pas le bon mot. Le mot était " + listeMots[2] + ". Votre score est de " + score)
+}

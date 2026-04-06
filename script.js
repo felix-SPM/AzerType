cf1348855161c197f62da987b08203cdbbe5005c
Felix Locquet <felix.locquet@outlook.fr>
2026-04-05 23:43:51 -0200

    Exercice des mots avec boucle for

--- script.js
+++ script.js
@@ -1,30 +1,15 @@
 const listeMots = ["Cachalot", "Pétunia", "Serviette"]
 
 let score = 0
-
-let motUtilisateur = prompt("Entrez le premier mot du tableau :")
-
-if (motUtilisateur === listeMots[0]) {
-    score++
-    alert("Bravo ! Vous avez trouvé le mot " + listeMots[0] + " ! Votre score est de " + score)
-} else {
-    alert("Dommage, ce n'est pas le bon mot. Le mot était " + listeMots[0] + ". Votre score est de " + score)
+for(i=0;i<=2;i++)
+{
+    let motUtilisateur = prompt("Entrez le mot "+listeMots[i]+": ")
+    if (motUtilisateur === listeMots[i])
+    {
+        score++
+    }
 }
 
-motUtilisateur = prompt("Entrez le deuxième mot du tableau :")
+console.log("Votre score est de "+score+"/3")
 
-if (motUtilisateur === listeMots[1]) {
-    score++
-    alert("Bravo ! Vous avez trouvé le mot " + listeMots[1] + " ! Votre score est de " + score)
-} else {
-    alert("Dommage, ce n'est pas le bon mot. Le mot était " + listeMots[1] + ". Votre score est de " + score)
-}
 
-motUtilisateur = prompt("Entrez le troisième mot du tableau :")
-
-if (motUtilisateur === listeMots[2]) {
-    score++   
-    alert("Bravo ! Vous avez trouvé le mot " + listeMots[2] + " ! Votre score est de " + score)
-} else {
-    alert("Dommage, ce n'est pas le bon mot. Le mot était " + listeMots[2] + ". Votre score est de " + score)
-}
